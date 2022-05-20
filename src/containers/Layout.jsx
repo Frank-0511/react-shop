5667c01d35e54997dbead9959cf1b641780711ae
Frank Villanueva <frank05111996@gmail.com>
2022-05-19 19:54:38 -0500

    :art: WIP

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import Header from "@components/Header";
 
 const Layout = ({ children }) => {
-  return (
-    <div className="Layout">
-      <Header />
-      {children}
-    </div>
-  );
+  return <div className="Layout">{children}</div>;
 };
 
 export default Layout;
